06a8ba896b3b458442976a2e1e6182371481ff69
shihyunchen <shihyunchen@users.noreply.github.com>
2018-03-15 21:32:09 +0800

    Initial commit

---
+++ app/src/main/java/com/cinna/learning201801/learning1_java.java
@@ -0,0 +1,13 @@
+package com.cinna.learning201801;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class learning1_java extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.learning1_xml);
+    }
+}
